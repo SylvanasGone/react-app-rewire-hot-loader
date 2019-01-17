71e0b3c7e0982df0cdb464a36b72f6fa37b0f243
sylvanasGone <397009765@qq.com>
2019-01-17 18:35:03 +0800

    refactor: transfer func

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const webpack = require('webpack')
-const { injectBabelPlugin } = require('./utils/injectBabelPlugin')
+
+const getLoader = (rules, matcher) => {
+  let loader
+
+  rules.some(rule => {
+    return (loader = matcher(rule)
+      ? rule
+      : getLoader(
+          rule.use || rule.oneOf || (Array.isArray(rule.loader) && rule.loader) || [],
+          matcher
+        ))
+  })
+
+  return loader
+}
+
+const getBabelLoader = rules => {
+  return getLoader(rules, babelLoaderMatcher)
+}
+
+const injectBabelPlugin = (pluginName, config) => {
+  const loader = getBabelLoader(config.module.rules)
+  if (!loader) {
+    console.log('babel-loader not found')
+    return config
+  }
+  // Older versions of webpack have `plugins` on `loader.query` instead of `loader.options`.
+  const options = loader.options || loader.query
+  options.plugins = [pluginName].concat(options.plugins || [])
+  return config
+}
+
 
 function rewireHotLoader(config, env) {
   if (env === 'production') {
